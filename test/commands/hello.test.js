ac7ab9ea39ae9be07293f2b80d337707e8d2f821
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-26 01:19:23 -0800

    fix: ran generator

--- test/commands/hello.test.js
+++ test/commands/hello.test.js
@@ -1,10 +1,15 @@
-const {expect, describe, testCommand} = require('@dxcli/dev-test')
+const {expect, test} = require('@dxcli/dev-test')
 
 describe('command', () => {
-  testCommand(['hello'], {stdout: true}, ({stdout}) => {
-    expect(stdout).to.equal('hello world!\n')
-  })
-  testCommand(['hello', '--name', 'jeff'], {stdout: true}, ({stdout}) => {
-    expect(stdout).to.equal('hello jeff!\n')
-  })
+  test
+    .stdout()
+    .command(['hello'])
+    .do(output => expect(output.stdout).to.contain('hello world!'))
+    .it('says hello')
+
+  test
+    .stdout()
+    .command(['hello', '--name', 'jeff'])
+    .do(output => expect(output.stdout).to.contain('hello jeff!'))
+    .it('says hello jeff')
 })
